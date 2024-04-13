bb1561233bc91623be94376b8067df64424e8fe5
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2024-04-13 21:06:09 +0200

    Fix random Unicode character

--- ebcdic.go
+++ ebcdic.go
@@ -21,7 +21,7 @@ func init() {
 	}
 }
 
-// DecodeEbcdic converts EBCDIC text in `bytes“ to UTF-8 string.
+// DecodeEbcdic converts EBCDIC text in `bytes` to UTF-8 string.
 func DecodeEbcdic(bytes []byte) string {
 	var result = make([]rune, 0, len(bytes))
 	for _, b := range bytes {
